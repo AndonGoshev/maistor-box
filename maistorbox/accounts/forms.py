19e8cfb50bdbe94617c67b7787c059cb6f694b3c
andongoshev <andon.go6ev@gmail.com>
2024-12-04 01:56:55 +0200

    Added custom template for PermissionDenied

--- maistorbox/accounts/forms.py
+++ maistorbox/accounts/forms.py
@@ -33,6 +33,8 @@ class BaseUserRegistrationForm(ErrorMessagesTranslateMixin, UserCreationForm, Fo
 
 
 
+
+
 class ContractorUserRegistrationForm(ErrorMessagesTranslateMixin, UserCreationForm, FormsStylingMixin):
     # Base user fields
     email = forms.EmailField(required=True)
