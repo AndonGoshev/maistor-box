22725dfbafd201ed2191d394c5cd736fb7820aec
andongoshev <andon.go6ev@gmail.com>
2024-12-04 11:58:44 +0200

    Fixed a syntax error in the search board urls

--- maistorbox/search_board/urls.py
+++ maistorbox/search_board/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from maistorbox.search_board.views import SearchBoardView
 
 urlpatterns = [
-    path('contractors-search', (SearchBoardView.as_view), name='search_board'),
+    path('contractors-search', SearchBoardView.as_view(), name='search_board'),
 ]
